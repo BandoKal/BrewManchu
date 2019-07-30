30c8114ad353e1984bb88e97e266556ee37f4a43
Jason Bandy <jason.bandy@hcahealthcare.com>
2019-07-30 08:49:36 -0500

    Initial Commit

---
+++ BrewManchu/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BrewManchu
+//
+//  Created by JDU9706 on 7/30/19.
+//  Copyright © 2019 bandokal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
