8d81e48899f8ee96a8fd65b044e12cf04eb60b8c
Jason Bandy <jason.bandy@hcahealthcare.com>
2019-07-31 11:21:29 -0500

    Adding MapView

---
+++ BrewManchu/MapView.swift
@@ -0,0 +1,32 @@
+//
+//  MapView.swift
+//  BrewManchu
+//
+//  Created by JDU9706 on 7/31/19.
+//  Copyright © 2019 bandokal. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    var coordinate: CLLocationCoordinate2D
+    
+    func makeUIView(context: Context) -> MKMapView {
+        return MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 36.275565, longitude: -86.6865119))
+    }
+}
+#endif
