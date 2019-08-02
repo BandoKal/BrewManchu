5e24115f51952b877314ef2fb12bb9e48352bde1
Jason Bandy <jason.bandy@hcahealthcare.com>
2019-08-02 14:55:53 -0500

    location stuff still needs work though

--- BrewManchu/BreweryListView.swift
+++ BrewManchu/BreweryListView.swift
@@ -7,8 +7,68 @@
 //
 
 import SwiftUI
+import CoreLocation
 
-struct BreweryListView: View {
+enum LocationError: Error {
+    case unableToReverseGeocodeCity
+    case serviceRestrictedOrDenied
+    case authStatusUnknown
+    case underlyingError(Error)
+}
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    var clManager = CLLocationManager()
+    var completion: (Result<String, LocationError>) -> Void = {_ in }
+    
+    
+    func doLocationUpdate(_ completion: @escaping (Result<String, LocationError>) -> Void) {
+        self.completion = completion
+        clManager.delegate = self
+        if CLLocationManager.locationServicesEnabled() {
+            clManager.desiredAccuracy = kCLLocationAccuracyBest
+            
+            let authStatus = CLLocationManager.authorizationStatus()
+            switch authStatus {
+            case .notDetermined:
+                clManager.requestWhenInUseAuthorization()
+            case .restricted, .denied:
+                self.completion(.failure(.serviceRestrictedOrDenied))
+            case .authorizedAlways, .authorizedWhenInUse:
+                clManager.requestLocation()
+            @unknown default:
+                self.completion(.failure(.authStatusUnknown))
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        doLocationUpdate(self.completion)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.completion(.failure(.underlyingError(error)))
+        print("Failed \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //Called a few times 
+        let geocoder = CLGeocoder()
+        if let loc = locations.last {
+            geocoder.reverseGeocodeLocation(loc) { (placemarks, error) in
+                guard let placemark = placemarks?.last,
+                    let city = placemark.locality else {
+                        self.completion(.failure(.unableToReverseGeocodeCity))
+                        return
+                }
+                self.completion(.success(city))
+            }
+        }
+    }
+}
+
+struct BreweryListView: View  {
+    
+    let locationManager = LocationManager()
     @ObservedObject var viewModel: ListViewModel = ListViewModel()
     
     var body: some View {
@@ -22,13 +82,19 @@ struct BreweryListView: View {
             }.padding()
         }
         .onAppear {
-            //TODO: Locatin Permission? (In use only)
-            // if no permission prompt for it.
-            // Find locality
-            // input locality in load
-            self.viewModel.loadLocations(for: "Nashville")
+            self.locationManager.doLocationUpdate { result in
+                switch result {
+                case .success(let city):
+                    self.viewModel.loadLocations(for: city)
+                case .failure(let error):
+                    //TODO: Prompt user for city to search or to allow location services
+                    print(error)
+                }
+            }
         }
     }
+    
+    
 }
 
 #if DEBUG
