05957798af5da021d8dc25c7fa6a6b41691bd007
Jason Bandy <jason.bandy@hcahealthcare.com>
2019-08-01 21:28:30 -0500

    ImageStore to get images from api

--- BrewManchu/ImageStore.swift
+++ BrewManchu/ImageStore.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum ImageName: String {
+    case defaultBeer = "default-beer"
+}
+
+
+func bundleImage(named name: ImageName, from bundle: Bundle = Bundle.main) -> Image {
+    return Image(name.rawValue, bundle: bundle)
+}
+
+func imagePublisherFromURLString(_ urlString: String) throws -> URLSession.DataTaskPublisher {
+    guard let url = URL(string: urlString) else { throw NetworkError.urlBuild }
+    return URLSession.shared.dataTaskPublisher(for: url)
+}
